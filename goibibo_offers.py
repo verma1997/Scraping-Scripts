f7bb6ce145a01bec20b8df4ec3ba9ecaac07192f
Priyanshu Verma <201652019@iiitvadodara.ac.in>
2019-05-20 20:51:10 +0530

    modified goibibo_offers.py

--- goibibo_offers.py
+++ goibibo_offers.py
@@ -12,59 +12,36 @@ page_soup = BeautifulSoup(html, "html.parser")
 
 offers = []
 
-containers = page_soup.findAll("div",{"class":"bot-section"})
-
-container_len = len(containers)
-
 filename = "goibibo_offers.csv"
 # headers = "Offers,Time\n"
-
-f = open("goibibo_offers.csv","a");
-
 # f.write(headers)
 
 def job():
-	f = open(filename, "a")	
+	containers = page_soup.findAll("div",{"class":"bot-section"})
+
+	container_len = len(containers)
+	
+	f = open(filename,"a")
+
 	for item in page_soup.select("[class^='sub-heading click-effects']"):
 		print(item.text + str(datetime.now()))
-		f.write(item.text + "\t" + str(datetime.now()) + "\n")
+		f.write(item.text + "\n")
 
-	print("\n")
+	for i in range(0,container_len):
+		print(containers[i].p["onclick"])
+		f.write(containers[i].p["onclick"] + str(datetime.now()) +"\n")	
+	
 	f.close()
 
 
 schedule.every(1).minutes.do(job)
-# schedule.every().day.at("18:43").do(job)
-# schedule.every().hour.do(job)
-# schedule.every().seconds.do(job)
+schedule.every().day.at("18:43").do(job)
+schedule.every().hour.do(job)
+schedule.every().seconds.do(job)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-
-# url = "https://www.goibibo.com/offers/"
-# url = "https://www.sastiticket.com/offers"
-
-#opening up connection and grabing the content
-# uClient = uReq(url)
-
-#dumping the content in a variable
-# page_html = uClient.read()
-
-# uClient.close()
-
-#html parser
-# page_soup = soup(page_html, "html.parser")
-
-# containers = page_soup.findAll("div",{"class":"bot-section"})
-
-# container_len = len(containers)
-
-# filename = "goibibo_offers.csv"
-# f = open(filename, "w")
-
-# headers = "Name\n"
-# f.write(headers)
 
 # for item in page_soup.select("[class^='sub-heading click-effects']"):
 # 	print(item.text)
