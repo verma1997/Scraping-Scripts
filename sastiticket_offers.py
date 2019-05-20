cf5adaf5bd28424caa6b50234e82cdea7b3d19e1
Priyanshu Verma <201652019@iiitvadodara.ac.in>
2019-05-20 20:40:33 +0530

    modified sastiticket_offers.py

--- sastiticket_offers.py
+++ sastiticket_offers.py
@@ -14,40 +14,32 @@ page_soup = BeautifulSoup(html, "html.parser")
 
 offers = []
 
-containers = page_soup.findAll("div",{"class":"col-lg-3 col-md-3 col-xs-12 col-sm-12"})
-
-image_links = page_soup.findAll("img",{"class":"img-responsive"})
-
-container_len = len(containers)
-
 filename = "sastiticket_offers.csv"
 
 img_links = []
 a_links = []
 
-headers = "Offer Image Link \n"
-f = open(filename,"a")
-f.write(headers)
-f.close()
-
 def job():
-	f = open(filename,"a")
+	containers = page_soup.findAll("div",{"class":"col-lg-3 col-md-3 col-xs-12 col-sm-12"})
+
+	image_links = page_soup.findAll("img",{"class":"img-responsive"})
 	
+	f = open(filename,"a")
+
+	for links in containers:
+		a = links.find('a')
+		a_links = a['href']
+		print(a_links)
+		f.write(a_links + "\n")
+	
+	f.write("\n")
+		
 	for link in image_links:
 		img_links = link['src']
 		print(img_links)
 		f.write(img_links + "\n")
 
-	print("\n")
-	print("\n")
-
-	for links in containers:
-		a = links.find('a')
-		a_links = a['href']
-		f.write(a_links + "\n")
 	f.close()
-
-		
 
 schedule.every(1).minutes.do(job)
 schedule.every().day.at("18:43").do(job)
