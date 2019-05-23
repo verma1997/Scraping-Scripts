ede62e48c99a5aa2ccf66dc74adddef57317836e
Priyanshu Verma <201652019@iiitvadodara.ac.in>
2019-05-23 10:10:23 +0530

    modified goibibo_offers.py

--- goibibo_offers.py
+++ goibibo_offers.py
@@ -8,55 +8,48 @@ import re
 import schedule
 
 html = urlopen("https://www.goibibo.com/offers/")
+
 page_soup = BeautifulSoup(html, "html.parser")
 
+container = page_soup.findAll("div",{"class":"bot-section"})
+
+promo_container = page_soup.findAll("div",{"class":"back-content"})
+
+total = len(container)
+
+
 offers = []
+promos = []
+links = []
+terms_conditions = []
 
-filename = "goibibo_offers.csv"
-# headers = "Offers,Time\n"
-# f.write(headers)
+for offer_names in container:
+	offers.append(offer_names.p.text) 
 
-def job():
-	containers = page_soup.findAll("div",{"class":"bot-section"})
-
-	container_len = len(containers)
+for content in promo_container:
 	
-	f = open(filename,"a")
-
-	for item in page_soup.select("[class^='sub-heading click-effects']"):
-		print(item.text + str(datetime.now()))
-		f.write(item.text + "\n")
-
-	for i in range(0,container_len):
-		print(containers[i].p["onclick"])
-		f.write(containers[i].p["onclick"] + str(datetime.now()) +"\n")	
+	link_text = content.a['href']
+	links.append("https://www.goibibo.com"+link_text)
 	
-	f.close()
+	promo_content = content.find_all("div",{"class":"back-content-top-inner"})
+	conditions = content.find_all("div",{"class":"tnc-section"})
+	
+	for terms in conditions:
+		terms_conditions.append(terms.li.text)
+		# print(terms.li.text)
+
+	for promo in promo_content:
+		promo_code = promo.find_all("div",{"class":"promocode"})
+		for promo_text in promo_code:
+			promos.append(promo_text.text)
+			# print(promo_text.text)
 
 
-schedule.every(1).minutes.do(job)
-schedule.every().day.at("18:43").do(job)
-schedule.every().hour.do(job)
-schedule.every().seconds.do(job)
+# schedule.every(1).minutes.do(job)
+# schedule.every().day.at("18:43").do(job)
+# schedule.every().hour.do(job)
+# schedule.every().seconds.do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-# for item in page_soup.select("[class^='sub-heading click-effects']"):
-# 	print(item.text)
-# 	f.write(item.text + "\n")
-
-# for i in range(0,container_len):
-# 	print(containers[i].p["onclick"])
-# 	f.write(containers[i].p["onclick"] + "\n")
-
-# f.close()
-
-# page_soup = soup(page_html, "html.parser")
-# links = []
-
-# for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
-# 	links.append(link.get('href'))
-
-# print(links)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
